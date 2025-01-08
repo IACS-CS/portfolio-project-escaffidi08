de68a6035956cc23d6cba8fd7b126845b2a66de4
escaffidi08 <ellie.scaffidi@innovationcharter.org>
2025-01-08 14:16:22 +0000

    first page CSS & start projects page

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ console.log("main.js loaded successfully!");
 /* Some sample JavaScript: this just listens 
 for any "click" on the hero element and then
 toggles the "active" class which we use to
-shift colors when they click */
+shift colors when they click 
 let hero = document.querySelector(".hero");
-hero.addEventListener("click", () => {
-  console.log("They clicked the hero!");
+hero.addEventListener("scroll", () => {
+  console.log("They scrolled!");
   hero.classList.toggle("active");
 });
+*/
